2b52875cd6906749d0b6903e5fe5154052b54750
Aniket Chaudhari <anikethc3@gmail.com>
2023-09-23 04:11:54 +0530

    added httpx and recon_filder

--- index.js
+++ index.js
@@ -19,13 +19,14 @@ app.post('/enumerate-subdomains', async (req, res) => {
     // Generate a unique ID for this task
     const generatedId = uuidv4();
 
-    // Add the task to the queue
-    const job = await subdomainQueue.add({ domain, generatedId });
+    // Add the task to the queue for subfinder
+    const subfinderJob = await subdomainQueue.add({ domain, generatedId, tool: 'subfinder' });
+
     // Respond immediately with the generatedId
     res.json({ generatedId });
 
-    // Start the subdomain enumeration task
-    startSubdomainEnumeration(domain, generatedId);
+    // Start the subdomain enumeration tasks
+    startSubdomainEnumeration(domain, generatedId, subfinderJob.id);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
@@ -35,40 +36,74 @@ app.post('/enumerate-subdomains', async (req, res) => {
 // Handle GET /enumeration-results
 app.get('/enumeration-results', async (req, res) => {
   try {
-    const { domain, generatedId } = req.query;
+    const { domain, generatedId, tool } = req.query;
 
-    // Define the file name based on domain and generatedId
-    const fileName = `${domain}_${generatedId}.txt`;
+    // Define the folder for output files
+    const outputFolder = 'recon_outputs';
+    const fileName = `${outputFolder}/${domain}_${generatedId}_${tool}.txt`;
 
     // Read and send the file content as response
     const fileContent = fs.readFileSync(fileName, 'utf-8');
     const arr = fileContent.split('\n');
-    return res.status(200).json({ result: arr})
+    return res.status(200).json({ result: arr });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-// Function to start subdomain enumeration task
-function startSubdomainEnumeration(domain, generatedId) {
-  const command = `subfinder -all -d ${domain} -o ${domain}_${generatedId}.txt`;
+// Function to start subdomain enumeration tasks
+function startSubdomainEnumeration(domain, generatedId, subfinderJobId) {
+  // Define the folder for output files
+  const outputFolder = 'recon_outputs';
 
-  const enumerationProcess = spawn('sh', ['-c', command]);
+  // Command for subfinder
+  const subfinderCommand = `subfinder -all -d ${domain} -o ${outputFolder}/${domain}_${generatedId}_subfinder.txt`;
+  
+  // Execute subfinder
+  const subfinderProcess = spawn('sh', ['-c', subfinderCommand]);
 
-  enumerationProcess.stdout.on('data', (data) => {
-    console.log(`Enumeration stdout: ${data}`);
+  // Handle stdout and stderr for subfinder
+  subfinderProcess.stdout.on('data', (data) => {
+    console.log(`Subfinder stdout: ${data}`);
   });
 
-  enumerationProcess.stderr.on('data', (data) => {
-    console.error(`Enumeration stderr: ${data}`);
+  subfinderProcess.stderr.on('data', (data) => {
+    console.error(`Subfinder stderr: ${data}`);
   });
 
-  enumerationProcess.on('close', (code) => {
+  // When subfinder task is finished, start HTTPx
+  subfinderProcess.on('close', (code) => {
     if (code === 0) {
-      console.log(`Enumeration process finished successfully.`);
+      console.log(`Subfinder process finished successfully.`);
+      startHttpx(domain, generatedId, outputFolder);
     } else {
-      console.error(`Enumeration process exited with code ${code}`);
+      console.error(`Subfinder process exited with code ${code}`);
+    }
+  });
+}
+
+// Function to start HTTPx
+function startHttpx(domain, generatedId, outputFolder) {
+  const inputFileName = `${outputFolder}/${domain}_${generatedId}_subfinder.txt`;
+  const httpxCommand = `httpx -l ${inputFileName} -title -status-code -content-length -ip -tech-detect -o ${outputFolder}/${domain}_${generatedId}_httpx.txt -t 100`;
+
+  const httpxProcess = spawn('sh', ['-c', httpxCommand]);
+
+  // Handle stdout and stderr for HTTPx
+  httpxProcess.stdout.on('data', (data) => {
+    console.log(`HTTPx stdout: ${data}`);
+  });
+
+  httpxProcess.stderr.on('data', (data) => {
+    console.error(`HTTPx stderr: ${data}`);
+  });
+
+  httpxProcess.on('close', (code) => {
+    if (code === 0) {
+      console.log(`HTTPx process finished successfully.`);
+    } else {
+      console.error(`HTTPx process exited with code ${code}`);
     }
   });
 }
